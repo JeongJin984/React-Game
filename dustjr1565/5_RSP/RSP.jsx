3fd01b609fafe1ecb7442fbee43fb820ef7450f8
chio yeon seo <gkwns9006@naver.com>
2020-07-16 16:43:12 +0900

    add RSP

---
+++ dustjr1565/5_RSP/RSP.jsx
@@ -0,0 +1,79 @@
+import React, {useState, useRef, useEffect} from 'react'
+
+const rspCoords = {
+    바위: '0',
+    가위: '-142px',
+    보: '-284px'
+}
+
+const scores = {
+    가위: 1,
+    바위: 0,
+    보: -1
+}
+
+const computerChoice = (imgCoord) => {
+    return Object.entries(rspCoords).find(function(v){
+        return v[1] === imgCoord
+    })[0]
+}
+
+const RSP = () => {
+    const [score, setScore] =useState(0)
+    const [result, setResult]=useState('')
+    const [imgCoord, setImgCoord]=useState(rspCoords.바위)
+    const interval = useRef()
+
+    useEffect(()=> {
+        interval.current = setInterval(changeHand, 100)
+        return () => {
+            clearInterval(interval.current)
+        }
+    }, [imgCoord])
+
+    const changeHand =() =>{
+        if(imgCoord === rspCoords.바위){
+            setImgCoord(rspCoords.가위)
+        }else if(imgCoord === rspCoords.가위){
+            setImgCoord(rspCoords.보)
+        }else{
+            setImgCoord(rspCoords.바위)
+        }
+    }
+
+    const onClickBtn = (choice) => ()=> {
+        clearInterval(interval.current)
+        const myScore = scores[choice]
+        const cpuScore = scores[computerChoice(imgCoord)]
+        const diff = myScore - cpuScore
+        if(diff===0){
+            setResult('비김')
+        }else if ([-1, 2].includes(diff)){
+            setResult('이김')
+            setScore((preScore)=>preScore+1)
+        }else {
+            setResult('짐')
+            setScore((preScore)=>preScore-1)
+        }
+
+        setTimeout(()=> {
+            clearTimeout(interval.current)
+            interval.current = setInterval(changeHand, 100)
+        }, 1000)
+    }
+
+    return(
+        <>
+            <div id="computer" style={{background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`}}/>
+            <div>
+                <button id="rock" className="btn" onClick={onClickBtn('바위')}>바위</button>
+                <button id="scissor" className="btn" onClick={onClickBtn('가위')}>가위</button>
+                <button id="paper" className="btn" onClick={onClickBtn('보')}>보</button>
+            </div>
+            <div>{result}</div>
+            <div>score: {score}</div>
+        </>
+    )
+}
+
+export default RSP;
