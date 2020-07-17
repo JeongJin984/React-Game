cf00fed0c85ae8729e5b01d4ffd644a31a67ae72
devtinatina <53737471+devtinatina@users.noreply.github.com>
2020-07-17 12:23:23 +0900

    change clinent.jsx [devtinatina]

--- devtinatina/4_responseCheck/client.jsx
+++ devtinatina/4_responseCheck/client.jsx
@@ -2,7 +2,7 @@ const React = require('react');
 const ReactDom = require('react-dom');
 const { hot } = require('react-hot-loader/root');
 
-const WordRelay = require('./WordRelay');
+const WordRelay = require('./responseCheck');
 
 const Hot = hot(WordRelay); // hoc
 
